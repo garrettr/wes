75780e169035e5428d9e93ff47e3450ebce9fd46
handsomeransoms <handsomeransoms@gmail.com>
2011-02-02 15:38:13 -0500

    Added a homepage redirection hack to urls.py.
    This fixes a previous, weird error where the request for the homepage
    spun around redirecting in a loop. This is better, but might get weird
    (how do I define subpages now?). Or maybe not.

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    # a little hack for making home work (for now)
+    (r'^$', 'wes.views.page', { 'url':'/home/' }),
     (r'^(?P<url>.*)$', 'wes.views.page'),
 )
