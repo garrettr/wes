1e7972e867fa5cbd2af088de0dca66ae82fbf411
handsomeransoms <handsomeransoms@gmail.com>
2011-02-01 19:00:49 -0500

    retab all files. PEP-8 ftw

--- models.py
+++ models.py
@@ -3,23 +3,23 @@ from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 
 class Page(models.Model):
-	# unique=True?
-	url = models.CharField(_('URL'), max_length=100, db_index=True)
-	title = models.CharField(_('title'), max_length=200)
-	content = models.TextField(_('content'), blank=True)
-	#enable_comments = models.BooleanField(_('enable comments'))
-	template_name = models.CharField(_('template name'), max_length=70, blank=True, help_text=_("Example: 'flatpages/contact_page.html'. If this isn't provided, the system will use 'flatpages/default.html'."))
-	#sites = models.ManyToManyField(Site)
+    # unique=True?
+    url = models.CharField(_('URL'), max_length=100, db_index=True)
+    title = models.CharField(_('title'), max_length=200)
+    content = models.TextField(_('content'), blank=True)
+    #enable_comments = models.BooleanField(_('enable comments'))
+    template_name = models.CharField(_('template name'), max_length=70, blank=True, help_text=_("Example: 'flatpages/contact_page.html'. If this isn't provided, the system will use 'flatpages/default.html'."))
+    #sites = models.ManyToManyField(Site)
 
-	# wes features
-	#is_draft = models.BooleanField(_('draft'))
-	sub_pages = models.ManyToManyField('Page', blank=True)
+    # wes features
+    #is_draft = models.BooleanField(_('draft'))
+    sub_pages = models.ManyToManyField('Page', blank=True)
 
-	class Meta:
-		ordering = ('url',)
+    class Meta:
+        ordering = ('url',)
 
-	def __unicode__(self):
-		return u"%s -- %s" % (self.url, self.title)
+    def __unicode__(self):
+        return u"%s -- %s" % (self.url, self.title)
 
-	def get_absolute_url(self):
-		return self.url
+    def get_absolute_url(self):
+        return self.url
--- views.py
+++ views.py
@@ -9,35 +9,35 @@ from django.utils.safestring import mark_safe
 DEFAULT_TEMPLATE = 'wes/default.html'
 
 def page(request, url):
-	"""
-	Public interface to the wes Page view
-	"""
-	if not url.endswith('/') and settings.APPEND_SLASH:
-		return HttpResponseRedirect("%s/" % request.path)
-	if not url.startswith('/'):
-		url = "/" + url
-	p = get_object_or_404(Page, url__exact=url, sites__id__exact=settings.SITE_ID)
-	return render_page(request, p)
+    """
+    Public interface to the wes Page view
+    """
+    if not url.endswith('/') and settings.APPEND_SLASH:
+        return HttpResponseRedirect("%s/" % request.path)
+    if not url.startswith('/'):
+        url = "/" + url
+    p = get_object_or_404(Page, url__exact=url, sites__id__exact=settings.SITE_ID)
+    return render_page(request, p)
 
 def render_page(request, p):
-	"""
-	Internal interface to the wes Page view
-	"""
+    """
+    Internal interface to the wes Page view
+    """
 
-	# Load template
-	if p.template_name:
-		t = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
-	else:
-		t = loader.get_template(DEFAULT_TEMPLATE)
+    # Load template
+    if p.template_name:
+        t = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
+    else:
+        t = loader.get_template(DEFAULT_TEMPLATE)
 
-	# Mark title and content as safe - are assumed to be raw HTML content anyway
-	# (to avoid having to always use the "|safe" filter in wes templates)
-	p.title = mark_safe(p.title)
-	p.content = mark_safe(p.content)
-	
-	c = RequestContext(request, {
-		'page': p,
-	})		
-	response = HttpResponse(t.render(c))
-	populate_xheaders(request, response, Page, p.id)
-	return response
+    # Mark title and content as safe - are assumed to be raw HTML content anyway
+    # (to avoid having to always use the "|safe" filter in wes templates)
+    p.title = mark_safe(p.title)
+    p.content = mark_safe(p.content)
+    
+    c = RequestContext(request, {
+        'page': p,
+    })      
+    response = HttpResponse(t.render(c))
+    populate_xheaders(request, response, Page, p.id)
+    return response
